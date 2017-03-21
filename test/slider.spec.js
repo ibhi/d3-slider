5caa410784b28ea42e75a280fd16da808b64741c
ibhi <ibrahimin2004@gmail.com>
2017-03-21 21:40:01 +0530

    chore(karma): unit testing with karma and jasmine

---
+++ test/slider.spec.js
@@ -0,0 +1,8 @@
+describe('test', () => {
+  it('true should be true', () => {
+    expect(true).toBe(true);
+  });
+  it('false should not be true', () => {
+    expect(false).toBeFalsy();
+  });
+});
